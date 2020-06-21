8429431a3887defcfedf9272fa3adc116595117f
Jackson Chen <jacksonchen171@gmail.com>
2020-06-21 03:03:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,7 @@
 # Add your code here
+def help
+  puts "- help : displays this help message"
+  puts "- list : displays al ist of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
